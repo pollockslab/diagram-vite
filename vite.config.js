e7cb0e60d15f1038500b8cc2c29e8588f45444d4
MyungHyun91 <kimchiman91@naver.com>
2025-12-24 06:53:42 +0900

    setting

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './',
   // 깃허브용은 확인:  base: '/diagram-vite/',
+  base: './',
+  build: {
+    outDir: 'docs',
+  },
 })
